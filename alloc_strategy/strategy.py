bd0ae7ec738db3c51453046e9b07f533c7168b59
Soulou <leo@unbekandt.eu>
2014-07-28 12:41:18 +0200

    Fix round robin

--- alloc_strategy/strategy.py
+++ alloc_strategy/strategy.py
@@ -20,19 +20,22 @@ class RandomAllocationStrategy(AllocationStrategy):
         return nodes[randint(0, len(nodes)-1)]
 
 class RoundRobinAllocationStrategy(AllocationStrategy):
-    def select_node(self, nodes):
-        if getattr(self, "nodes", None) == None:
-            self.nodes = nodes
-            self.current_index = 0
-        elif len(self.nodes) != len(nodes):
-            print("List of nodes changed, reset round-robin")
-            self.nodes = nodes
-            self.current_index = 0
+    __nodes = None
+    __current_index = 0
 
-        node = self.nodes[self.current_index]
-        self.current_index += 1
-        if self.current_index == len(self.nodes):
-            self.current_index = 0
+    def select_node(self, nodes):
+        if RoundRobinAllocationStrategy.__nodes == None:
+            RoundRobinAllocationStrategy.__nodes = nodes
+            RoundRobinAllocationStrategy.__current_index = 0
+        elif len(RoundRobinAllocationStrategy.__nodes) != len(nodes):
+            print("List of nodes changed, reset round-robin")
+            RoundRobinAllocationStrategy.__nodes = nodes
+            RoundRobinAllocationStrategy.__current_index = 0
+
+        node = RoundRobinAllocationStrategy.__nodes[RoundRobinAllocationStrategy.__current_index]
+        RoundRobinAllocationStrategy.__current_index += 1
+        if RoundRobinAllocationStrategy.__current_index == len(RoundRobinAllocationStrategy.__nodes):
+            RoundRobinAllocationStrategy.__current_index = 0
         return node
 
 AllocationStrategy.register(RandomAllocationStrategy)
